61cdd6834cfd24d6f56e2c12568b98486caf7f50
Rafael Leal <rpsantos@iportsolutions.com.br>
2022-09-15 13:48:18 -0300

    Commit 150922

--- App01_LayoutXF/App01_LayoutXF/MainPage.xaml.cs
+++ App01_LayoutXF/App01_LayoutXF/MainPage.xaml.cs
@@ -14,5 +14,30 @@ namespace App01_LayoutXF
         {
             InitializeComponent();
         }
+
+        private void IrPaginaStack_Clicado(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Stack.PaginaStack());
+        }
+
+        private void IrPaginaGrid_Clicado(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Grid.PaginaGrid());
+        }
+
+        private void IrPaginaAbsolute_Clicado(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Absolute.PaginaAbsolute());
+        }
+
+        private void IrPaginaRelative_Clicado(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Relative.PaginaRelative());
+        }
+
+        private void IrPaginaScroll_Clicado(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Scroll.PaginaScroll());
+        }
     }
 }
